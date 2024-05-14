0da4a347fba7b8229aba08470000998f67b097cd
chae1n <165850154+chae1n@users.noreply.github.com>
2024-05-14 09:41:19 -0700

    Add files via upload

---
+++ json.py
@@ -0,0 +1,11 @@
+#
+import json
+
+data = {
+
+    'name': 'John Doe',
+    'age': 30,
+    'city': 'New York, NY',
+    'interests': ['Golf', 'Running', 'Football', 'Traveling'],
+    'is_student': False
+}
